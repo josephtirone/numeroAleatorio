d23c60f664d3b3d8d6ff0549754cf3c4c9896d1d
Joseph Tirone <hugo.campos@electricmonkeys.games>
2023-12-18 11:17:51 -0300

    projeto finalizado

---
+++ app/reconhecimentoDeVoz.js
@@ -0,0 +1,29 @@
+const elementoChute = document.getElementById('chute');
+
+window.SpeechRecognition = window.SpeechRecognition || 
+window.webkitSpeechRecognition;
+
+  const recognition = new SpeechRecognition();
+
+  recognition.lang = 'pt-Br'
+
+  recognition.start();
+
+recognition.addEventListener('result', onSpeak)
+
+const numeroFalado = document.getElementsByClassName('box');
+
+function onSpeak(e){
+    chute = e.results[0][0].transcript
+    exibeChuteNaTela(chute);
+    verficaValorValido(chute);
+}
+
+function exibeChuteNaTela(chute){
+    elementoChute.innerHTML = `
+    <div>Você disse</div>
+    <span class="box">${chute}</span>
+    `
+}
+
+recognition.addEventListener('end', () => recognition.start())
